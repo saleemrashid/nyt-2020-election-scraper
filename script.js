04591128d9ba50254d3331d9f23accca9d902700
Saleem Rashid <dev@saleemrashid.com>
2020-11-05 23:48:07 +0000

    script: update axes

--- script.js
+++ script.js
@@ -41,6 +41,9 @@ for (const [state, record] of Object.entries(data)) {
     }), {
         title: `${state} - ${record.name}`,
         hovermode: "closest",
+        xaxis: {
+            range: [record.timestamps[0], new Date()],
+        },
         yaxis: {
             tickformat: ".2%",
         },
